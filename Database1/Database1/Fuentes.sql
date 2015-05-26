29dc0dd28a33dffd22ed273b0574d7199fc9f889
sergiosanccas <sergio.tendi@hotmail.com>
2015-05-26 12:50:55 +0200

    sergio

---
+++ Database1/Database1/Fuentes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Fuentes]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Coordenadas] NVARCHAR(50) NOT NULL, 
+    [Valoracion] INT NULL, 
+    [Temperatura] NCHAR(10) NOT NULL, 
+    [Caudal] INT NULL
+)

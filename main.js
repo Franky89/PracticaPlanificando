e75399e63fa69e8dc7950ebb2e5999eca9052038
sergiosanccas <sergio.tendi@hotmail.com>
2015-05-28 12:53:07 +0200

    Revert "htmlUpdate"
    This reverts commit c74c3133e431c55157639336fa61ca260a324bc8.

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 // JavaScript source code
 var map = null;
 var popup = L.popup();
-var markerMe = null;
-var newMark = null;
-var posi = null;
 
 $(document).ready(function () {
     map = L.map('mapa').setView([40.51298, -3.34954], 10);
@@ -32,73 +29,22 @@ $(document).ready(function () {
     $(window).resize();
 
     map.on('click', onMapClick);
-	//map.on('dblclick',posMe);
    
 });
 function onMapClick(e) {
-    posi=e;
-    $('#Coordenadas').attr('value', (e.latlng.lng+'+'+e.latlng.lat));
+    
+    $('#Coordenadas').attr('value', e.latlng.toString());
    
     popup
         .setLatLng(e.latlng)
    
         .setContent("As pinchado en: " + e.latlng.toString())
         .openOn(map);
-      }
+   
+   
 
-
-function markMe(position)
-{
-	markerMe = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
-	markerMe.bindPopup("¡Estoy aquí (" + position.coords.latitude + ", " + position.coords.longitude + ")!");
-	map.setView([position.coords.latitude, position.coords.longitude], 16);
-	$('#locate').addClass("pure-button pure-button-success");	
 }
 
-function showError(error)
-{    
-    var message = null;  
-    
-	if (error.core == error.PERMISSION_DENIED)
-		message = "El usuario no ha concedido los privilegios de geolocalización.";  
-	else if (error.core == error.POSITION_UNAVAILABLE)
-		message = "Posicion no disponible.";  
-	else if (error.core == error.TIMEOUT)
-		message = "Demasiado tiempo intentando obtener la localización del usuario.";  
-	else 
-		message = "No se ha podido geolocalizar con la configuración de su navegador.";  
 
-	$('#locate').addClass("pure-button pure-button-error");
-	alert(message);
-}  
 
-function geoMe()
-{
-	if (map !== null)
-	{
-		 if (navigator.geolocation)
-		 { 
-		 	if (markerMe !== null)
-				map.removeLayer(markerMe);
-
-			var queryOptions  = {timeout:5000, maximumAge:20000, enableHighAccurace:false};
-  			navigator.geolocation.getCurrentPosition(markMe, showError, queryOptions);	
-		}
-		else
-		{
-			$('#locate').addClass("pure-button pure-button-error");	
-			alert("La geolocalizción HTML5 no esta disponible en su navegador.");
-		}
-	}
-}
-
-function posMe(){
-	var name = $('#name').Attr('value');
-	newMark = L.marker([posi.latlng.lat, posi.latlng.lng]).addTo(map)
-	newMark.bindPopup("¡Fuente en (" + posi.latlng.lat + ", " + posi.latlng.lng + ")!"+"(Nombre :"+name+")");
-	map.setView([posi.latlng.lat, posi.latlng.lng], 16);
-	
-
-	
-}
 
